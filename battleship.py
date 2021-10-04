63a8b9ed5940fe4e07a9127fb2cbadb68ef2fc4e
Phani9231 <phanibeechani@gmail.com>
2021-10-04 11:43:20 +0530

    Implemented IsHorizontal

--- battleship.py
+++ battleship.py
@@ -165,7 +165,11 @@ Parameters: 2D list of ints
 Returns: bool
 '''
 def isHorizontal(ship):
-    return
+    ship.sort()
+    if ship[0][0]==ship[2][0] and ship[1][0]==ship[2][0] :
+        if ship[0][1]==ship[1][1]-1 and ship[2][1]==ship[1][1]+1 :
+            return True
+    return False
 
 
 '''
